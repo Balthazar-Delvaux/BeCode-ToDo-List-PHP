903c9b9930a252a10e960d53958d17a2c29850bc
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2019-08-26 15:21:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-do List!</title>
+    <link rel="stylesheet" href="assets/css/foundation.min.css">
+</head>
+
+<body>
+    <section class="tasks">
+        <h3>TO DO</h3>
+        <h3>ARCHIVE</h3>
+    </section>
+    <section class="form">
+        <h2>Add a task</h2>
+        <h3>TASK TO DO</h3>
+        <form action="" method="post">
+            <input type="text" name="" id="">
+        </form>
+    </section>
+    <!-- Scripts -->
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/what-input.js"></script>
+    <script src="assets/js/vendor/foundation.min.js"></script>
+</body>
+
+</html>
